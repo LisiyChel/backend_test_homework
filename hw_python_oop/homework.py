40f4eb59140313d38ff96f3c8d3d562a439ba599
Иван Воробьев <hosts_zex@mail.ru>
2022-10-03 11:26:27 +0500

    5

--- hw_python_oop/homework.py
+++ hw_python_oop/homework.py
@@ -1,6 +1,11 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self, training_type:str, duration:float, distance:float, speed:float, calories:float)-> None:
+
+    def __init__(self, training_type:str,
+                 duration:float,
+                 distance:float,
+                 speed:float,
+                 calories:float)-> None:
         self.training_type = training_type
         self.duration = duration
         self.distance = distance
@@ -33,28 +38,34 @@ class Training:
     
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
+
         distance = self.action * self.LEN_STEP / self.M_IN_KM
         return distance
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
+
         return self.get_distance() / self.duration        
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
+
         pass
    
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
+
         return InfoMessage(type(self).__name__,
                            self.duration,
                            self.get_distance(),
                            self.get_mean_speed(),
-                           self.get_spent_calories())        
+                           self.get_spent_calories()
+                           )        
 
 
 class Running(Training):
     """Тренировка: бег."""
+
     def __init__(self, action: int, duration: float, weight: float) -> None:
          super().__init__(action, duration, weight)  
    
@@ -65,7 +76,8 @@ class Running(Training):
                 * self.weight / Training.M_IN_KM * (self.duration*Training.MIN_IN_HOUR))
 
 class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""    
+    """Тренировка: спортивная ходьба."""
+
     def __init__(self, action, duration, weight, height:float):
         super().__init__(action, duration, weight)
         self.height = height
@@ -99,6 +111,7 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
+
     get_information  = {
         'SWM': Swimming,
         'RUN': Running,
@@ -108,6 +121,7 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training) -> None:
     """Главная функция."""
+
     info = training.show_training_info()
     print(info.get_message())
 
@@ -122,4 +136,4 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-
+        
